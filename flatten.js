df5a1732288b7182100d52f3f03f41e498740bb2
Gareth <gar.carey@gmail.com>
2021-07-27 16:02:44 -0400

    flatten contents on nested arrays into a single array

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if(eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅✅✅Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  let counter = 0;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] === secondArray[i]) {
+      counter ++;
+    }
+  }
+  if (counter === firstArray.length && counter === secondArray.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const flatten = function(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])){
+      for (let j = 0; j < array[i].length; j++) {
+        newArray.push(array[i][j]);
+      }
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+};
+
+flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
