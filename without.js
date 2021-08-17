7cf3a7fa3496dc1f9dfc62bd25e4a17a64d75e55
Gareth <gar.carey@gmail.com>
2021-08-17 18:55:13 -0400

    add if statement to determine if itemsToBeRemoved is a string and convert to array

--- without.js
+++ without.js
@@ -21,18 +21,28 @@ const assertArraysEqual = function(arrayOne, arrayTwo) {
 };
 
 const without = function(source, itemsToRemove) {
+  let remove = [];
+  if (typeof(itemsToRemove) === "string"){
+    remove = itemsToRemove.split(' ');
+  } else {
+    remove = itemsToRemove;
+  }
+
   let returnArr = [];
-  for (let i = 0; i < source.length; i++) {
-    if (source[i] !== itemsToRemove) {
-        returnArr.push(source[i]);
+  for (let i = 0; i < remove.length; i++) {
+    for (let j = 0; j < source.length; j++){
+      if (remove[i] !== source[j] && !returnArr.includes(source[j])) {
+        returnArr.push(source[j]);
+      }
     }
   }
   return returnArr;
 };
 
 console.log(without(["hello", "world", "lighthouse"], "lighthouse"));
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 
 const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
+console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
